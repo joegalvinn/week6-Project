08799e31c4b503694d66cada887e19e99ff226a0
Joseph <joseph.galvin@digisolve.co.uk>
2024-10-04 14:20:24 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+//i am goign to write all my notes in this component,
+// you should divide my notes and tasks into different components whener possible
+import { StrictMode } from "react";
+import Header from "./components/Header";
+
+export default function app() {
+  return (
+    <>
+      <Header />
+
+      <h1>Cookies</h1>
+    </>
+  );
+}
